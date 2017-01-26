acbb9588f81a70ca2d0a3cfb900a8a5e8c17393a
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2017-01-26 14:11:43 -0800

    day 28 - 1

---
+++ day-28/cfgram-auth/karma.conf.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// step 7
+const webpack = require('./webpack.config.js');
+webpack.entry = {};
+
+
+module.exports = function(config) {
+  config.set({
+    // step 8
+    webpack,
+
+    basePath: '',
+
+    frameworks: ['jasmine'],
+
+    files: [
+      // step 10
+      'app/entry.js',
+      'test/**/*-test.js',
+      'node_modules/angular-mocks/angular-mocks.js'
+    ],
+
+    exclude: [
+      // possibly could include node_modules here.
+    ],
+
+    preprocessors: {
+      // step 9
+      'test/**/*-test.js': ['webpack'],
+      'app/entry.js': ['webpack']
+    },
+
+    reporters: ['progress'],
+    
+    port: 9876,
+
+    colors: true,
+
+    logLevel: config.LOG_INFO,
+
+    autoWatch: true,
+
+    browsers: ['PhantomJS'],
+
+    singleRun: false,
+
+    concurrency: Infinity
+  });
+};
---
+++ day-28/cfgram-auth/webpack.config.js
@@ -0,0 +1,78 @@
+'use strict';
+
+// step 1
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+const HTMLPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// step 2 - loads in env vars
+dotenv.load();
+
+// step 3
+const production = process.env.NODE_ENV === 'production';
+
+// step 4
+let plugins = [
+  new ExtractTextPlugin('bundle.css'),
+  new HTMLPlugin({
+    template: `${__dirname}/app/index.html`
+  }),
+  new webpack.DefinePlugin({
+    __API_URL__: JSON.stringify(process.env.API_URL),
+    __DEBUG__: JSON.stringify(!production),
+  })
+];
+
+// step 5
+if(production) {
+  plugins = plugins.concat([
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      compress: {
+        warning: false
+      }
+    }),
+    new CleanPlugin()
+  ]);
+}
+
+// step 6
+module.exports = {
+  entry: `${__dirname}/app/entry.js`,
+  devtool: production ? false : 'eval',
+  plugins,
+  output: {
+    path: 'build',
+    filename: 'bundle.js'
+  },
+  sassLoader: {
+    includePaths: [`${__dirname}/app/scss`]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html'
+      },
+      {
+        test: /\.(woff|ttf|svg|eot).*/,
+        loader: 'url?limit=10000&name=font/[hash].[ext]'
+      },
+      {
+        test: /\.(jpg|jpeg|svg|bmp|tiff|gif|png)$/,
+        loader: 'url?limit=10000&name=image/[hash].[ext]'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css!resolve-url!sass?sourceMap')
+      }
+    ]
+  }
+};
